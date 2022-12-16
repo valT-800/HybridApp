7a3c7981778bf6d70ccfb88741e00b64c76457d0
valT-800 <65894951+valT-800@users.noreply.github.com>
2022-12-16 11:10:52 +0200

    completed

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import app from './firebaseConfig'
+import Styles from './Styles';
+import ScanItem from './src/screens/ScanItem';
+import ItemScreen from './src/screens/ItemScreen';
+import ItemsScreen from './src/screens/ItemsScreen';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+function Navigation(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={ItemScreen}>
+          <Stack.Screen name = "Items" component={ItemsScreen} options = {{headerStyle: Styles.header, title: ''}}/>
+          <Stack.Screen name = 'ScanItem' component={ScanItem} options = {{headerStyle: Styles.header, title: ''}}/>
+          <Stack.Screen name = "Item" component={ItemScreen} options = {{title: '', headerStyle: Styles.header}}/>   
+        </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+function App(){
+  return(
+    <Navigation/>
+    
+  )
+}
+
+export default App;
